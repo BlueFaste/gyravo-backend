83f6f56ed78319fa697de8ce72f2ec107f2f2e82
Caroline Fassot <fassot.caroline@gmail.com>
2022-04-17 18:44:57 +0200

    Fix param's router

--- router/admin/courses.js
+++ router/admin/courses.js
@@ -10,8 +10,8 @@ router.use(function timeLog(req, res, next) {
 router.get('/', function(req, res) {
   res.send('Listing s of courses page');
 });
-router.get('/:courseName', function(req, res) {
-  res.send('A page of a course: ' + req.params.courseName);
+router.get('/:idCourse', function(req, res) {
+  res.send('A page of a course: ' + req.params.idCourse);
 });
 
 router.post('/', function(req, res){
@@ -22,8 +22,8 @@ router.put('/:idCourse', function(req, res){
   res.send('Update a post')
 })
 
-router.delete('/:courseName', function(req, res){
-  res.send('Delete the course : ' + req.params.courseNameoo)
+router.delete('/:idCourse', function(req, res){
+  res.send('Delete the course : ' + req.params.idCourse)
 })
 
 export default router; 
--- router/admin/members.js
+++ router/admin/members.js
@@ -6,16 +6,20 @@ router.get('/', function(req, res) {
   res.send('Listing s of office members page');
 });
 
+router.get('/:idMember', function(req, res) {
+  res.send('A page of a member: ' + req.params.idMember);
+});
+
 router.post('/', function(req, res){
   res.send('Add a member')
 })
 
-router.put('/:idCourse', function(req, res){
-  res.send('Update a member : ' + req.params.idCourse)
+router.put('/:idMember', function(req, res){
+  res.send('Update a member : ' + req.params.idMember)
 })
 
-router.delete('/:idCourse', function(req, res){
-  res.send('Delete a member : ' + req.params.idCourse)
+router.delete('/:idMember', function(req, res){
+  res.send('Delete a member : ' + req.params.idMember)
 })
 
 export default router; 
